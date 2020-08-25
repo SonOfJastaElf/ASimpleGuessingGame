65d8953eb0913f285d180ea17961991b45c96de1
Brian Henderson <henderson.brian82@gmail.com>
2020-08-25 12:27:46 -0400

    Guessing Game Added

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace GuessingGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("I'm thinking of a number between 1 and 1000.  Can you guess what it is?");
+            
+            var r = new Random();
+            var computerNumber = r.Next(1, 1000);
+
+            Console.WriteLine("Give it a try:");
+
+            var userInput = int.Parse(Console.ReadLine());
+
+            if (userInput > computerNumber)
+            {
+                Console.WriteLine("That's too high.");
+            }
+
+            else if (userInput < computerNumber)
+            {
+                Console.WriteLine("That's too low.");
+            }
+
+            else
+            {
+                Console.WriteLine("Exactly!");
+            }
+        }
+    }
+}
